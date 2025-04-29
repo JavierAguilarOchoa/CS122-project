a55b9753f12751e17ce922634ece856d5002dca3
Javier <aguilarjaviii14@gmail.com>
2025-04-28 19:16:26 -0700

    Updated backend, added authorization and added Hardik's gui code.

--- backend/database.py
+++ backend/database.py
@@ -10,21 +10,24 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String, nullable=False)
     money = Column(Float, nullable=False)
+    # Maybe a password?
 
     def __repr__(self):
-        return f"<Book(id='{self.id}', ' name='{self.name}', ' money={self.money}')>"
+        return f"<User(id='{self.id}', ' name='{self.name}', ' money={self.money}')>"
 
 class Transaction(Base):
     __tablename__ = 'transactions'
 
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
+    id = Column(Integer, primary_key=True, autoincrement=True) # Transaction id
+    user_id = Column(Integer, ForeignKey('users.id')) # This is the transactions user id(The id of who made the transaction)
     amount = Column(Float, nullable=False)
     type = Column(String, nullable=False) # 'income' or 'expense'
     date = Column(Date, default=datetime.date.today(), nullable=False)
+
+    user = relationship("User", back_populates="transactions")
 
     def __repr__(self):
         return f"<Transaction(id='{self.id}', ' amount={self.amount}, type={self.type})>"
@@ -37,37 +40,43 @@ Session = sessionmaker(bind=engine)
 
 
 # ----------Database functions for user----------
-def add_user(user_id, user_name, user_money):
+def add_user(user_name, user_money):
     # INSERT INTO
     with Session() as session:
-        user = session.query(User).filter(User.id == user_id).first()
-        if user:
-            return False, "User already exists!"
-        new_user = User(id=user_id, name=user_name, money=0) # Money might start from 0 or user_money
-        session.add(new_user)
-        session.commit()
-        return True, f"User ({User.name},{User.id}) added Successfully!"
+        try:
+            new_user = User(name=user_name, money=user_money) # Money might start from 0 or user_money
+            session.add(new_user)
+            session.commit()
+            return True, f"User ({new_user.name},{new_user.id}) added Successfully!"
+        except Exception as e:
+            return False, f"Add user failed, due to {e}"
 
 def update_user(user_id, user_name, user_money): # Maybe we just want to use this to update the money
     # UPDATE
     with Session() as session:
-        user = session.query(User).filter(User.id == user_id).first()
-        if not user:
-            return False, "User not found!"
-        user.name = user_name
-        user.money = user_money
-        session.commit()
-        return True, f"User ({User.name},{User.id}) updated successfully!"
+        try:
+            user = session.query(User).filter(User.id == user_id).first()
+            if not user:
+                return False, "User not found!"
+            user.name = user_name
+            user.money = user_money
+            session.commit()
+            return True, f"User ({user.name},{user.id}) updated successfully!"
+        except Exception as e:
+            return False, f"Update user failed, due to {e}"
 
 def delete_user(user_id):
     # DELETE
     with Session() as session:
-        user = session.query(User).filter(User.id == user_id).first()
-        if not user:
-            return False, f"User:{user_id} does NOT exist!"
-        session.delete(user)
-        session.commit()
-        return True, f"User ({User.name},{User.id}) deleted successfully!"
+        try:
+            user = session.query(User).filter(User.id == user_id).first()
+            if not user:
+                return False, f"User:{user_id} does NOT exist!"
+            session.delete(user)
+            session.commit()
+            return True, f"User ({user.name},{user.id}) deleted successfully!"
+        except Exception as e:
+            return False, f"Delete user failed, due to {e}"
 
 def get_users():
     with Session() as session:
@@ -76,29 +85,38 @@ def get_users():
             print(user)
         return users
 
-# ----------Database functions for transactions----------
-def add_trans(trans_id, trans_user_id, new_amount, trans_type, trans_date):
+# ----------Database functions for transactions----------(Might just want the add/look over this as project implementation continues)
+def add_transaction(trans_user_id, new_amount, trans_type, trans_date): #TODO make sure that trans_user_id does not have to be inputted from a user's pov(Under the hood)
     # INSERT INTO
     with Session() as session:
-        if session.query(Transaction).filter(Transaction.id == trans_id, Transaction.user_id == trans_user_id).first():
-            return False, "Transaction already exists!"
-        new_trans = Transaction(id=trans_id, user_id=trans_user_id, amount=new_amount, type=trans_type, date=trans_date) # Money might start from 0 or user_money
-        session.add(new_trans)
-        session.commit()
-        return True, f"Transaction {trans_id} added Successfully!"
+        try:
+            new_trans = Transaction(
+                user_id=trans_user_id,
+                amount = new_amount,
+                type = trans_type,
+                date = trans_date
+            )
+            session.add(new_trans)
+            session.commit()
+            return True, f"Transaction added Successfully! ID:  {new_trans.id}"
+        except Exception as e:
+            return False, f"Transaction failed due to {e}"
 
-def delete_trans(trans_id, trans_user_id): #TODO make sure that trans_user_id does not have to be inputted from a user's pov(Under the hood)
+def delete_transaction(trans_id, trans_user_id): #TODO make sure that trans_user_id does not have to be inputted from a user's pov(Under the hood) Might not need this method
     # DELETE
     with Session() as session:
-        transaction = session.query(Transaction).filter(Transaction.id == trans_id, Transaction.user_id == trans_user_id).first()
-        if not transaction:
-            return False, "Transaction does NOT exist!"
-        session.delete(transaction)
-        session.commit()
-        return True, f"Transaction {trans_id} deleted successfully!"
+        try:
+            transaction = session.query(Transaction).filter(Transaction.id == trans_id, Transaction.user_id == trans_user_id).first()
+            if not transaction:
+                return False, "Transaction does NOT exist!"
+            session.delete(transaction)
+            session.commit()
+            return True, f"Transaction {trans_id} deleted successfully!"
+        except Exception as e:
+            return False, f"Delete transaction failed, due to {e}"
 
-def get_users_transactions(user_id):
+# Gets a users transactions
+def get_transactions(user_id):
     with Session() as session:
-        transactions = []
-        #TODO implement function
+        transactions = session.query(Transaction).filter(Transaction.user_id == user_id).all()
         return transactions
--- tests.py
+++ tests.py
@@ -1,15 +1,27 @@
 #Todo implement test cases & Include proper documentation in the form of docstrings for classes, functions, and methods.
 import unittest
+from backend.database import add_transaction, get_transactions, add_user, add_transaction, get_transactions, delete_user, get_users
+from datetime import datetime
 
-class MyTestCase(unittest.TestCase):
+class TestBudgetBuddy(unittest.TestCase):
     def setUp(self):
-        return
+        success, _ = add_user("TestUser",0)
+        self.test_user = [user for user in get_users() if user.name == "TestUser"][-1]
+
+    def tearDown(self):
+        delete_user(self.test_user.id)
 
     def test_add_user(self):
         return
 
     def test_add_transaction(self):
         return
+
+    def test_add_and_get_transaction(self):
+        add_transaction(self.test_user.id, 25, "Income", datetime.today().date())
+        transactions = get_transactions(self.test_user.id)
+        self.assertTrue(any(t.amount == 25 and t.type == "Income" for t in transactions))
+
 
 if __name__ == '__main__':
     unittest.main()
